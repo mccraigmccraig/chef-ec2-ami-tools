a098d8333f34ed3911c6f8c11c14f66aba649407
Richard Bullington-McGuire <rbullington-mcguire@bstonetech.com>
2012-06-15 12:29:47 -0400

    Refactor to make install root an attribute

---
+++ attributes/default.rb~
@@ -0,0 +1,1 @@
+default['chef-ec2-ami-tools']['install_root'] = "/usr/local/share"
--- recipes/default.rb
+++ recipes/default.rb
@@ -10,6 +10,9 @@ require 'fileutils'
 
 package "unzip"
 
+install_root = node["ec2-ami-tools"]["install_root"]
+install_target = "#{install_root}/ec2-ami-tools"
+
 remote_file "/tmp/ec2-ami-tools.zip" do
   source "http://s3.amazonaws.com/ec2-downloads/ec2-ami-tools.zip"
 end
@@ -17,17 +20,17 @@ end
 bash "unzip ec2-ami-tools" do
   cwd "/tmp"
   code <<-EOH
-    unzip -o -d /usr/local/share ./ec2-ami-tools.zip
+    unzip -o -d #{install_root} ./ec2-ami-tools.zip
   EOH
-  not_if { File.exist?("/usr/local/share/ec2-ami-tools") }
+  not_if { File.exist?(install_target) }
 end
 
 # create a soft-link to a known path only if the zipfile extracted
 ruby_block "link-ec2-ami-tools" do
   block do
-    if Dir["/usr/local/share/ec2-ami-tools-*"].first
-      FileUtils.rm_f("/usr/local/share/ec2-ami-tools")
-      FileUtils.ln_s(Dir["/usr/local/share/ec2-ami-tools-*"].first, "/usr/local/share/ec2-ami-tools")
+    if Dir["#{install_target}-*"].first
+      FileUtils.rm_f(install_target)
+      FileUtils.ln_s(Dir["#{install_target}-*"].first, install_target)
     end
   end
 end
