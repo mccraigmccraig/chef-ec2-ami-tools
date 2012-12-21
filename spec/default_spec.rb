1303949d2f541d986683bfcf212d92a49b1cfb5d
Anuj Biyani <anujbiyani01@gmail.com>
2012-12-20 18:03:40 -0800

    add spec for downloading ec2-ami-tools.zip

--- spec/default_spec.rb
+++ spec/default_spec.rb
@@ -6,4 +6,8 @@ describe 'chef-ec2-ami-tools::default' do
   it "installs unzip" do
     chef_run.should install_package 'unzip'
   end
+
+  it "downloads ec2-ami-tools.zip" do
+    chef_run.should create_remote_file '/tmp/ec2-ami-tools.zip'
+  end
 end
