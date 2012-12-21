946d6e357ba21e2fbffae2cf5b5c06c65214f13b
Anuj Biyani <anujbiyani01@gmail.com>
2012-12-21 13:05:25 -0800

    make an attribute for install_target set EC2_HOME environment variable and add ec2-ami-tools/bin to PATH

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,1 +1,3 @@
-default['chef-ec2-ami-tools']['install_root'] = "/usr/local/share"
+install_root = "/usr/local/share"
+default['chef-ec2-ami-tools']['install_root'] = install_root
+default['chef-ec2-ami-tools']['install_target'] = "#{install_root}/ec2-ami-tools"
